4f415b7f1a6eecd13a80b59d279fa94dad22790b
Ziwei Gu <zg48@cornell.edu>
2020-03-21 14:55:50 -0400

    initial commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from bokeh.embed import server_document
+
+application = Flask(__name__)
+
+@application.route("/")
+def index():
+    tag = server_document(url=r'/bokeh', relative_urls=True)
+    return render_template('index.html', tag=tag)
